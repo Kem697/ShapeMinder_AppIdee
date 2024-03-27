4f7415a52998d3814dc8eaf8eadf9124e9f1cb34
Kem697 <152178750+Kem697@users.noreply.github.com>
2024-03-27 16:01:26 +0100

    Current changes: -I implemented the code to change the status of the saveBtn. It is not working yet.
    Plans for continuing changes:
    -Finish the logic for the saveBtn.
    -Solve the delays, when the user uses the sort option and clicks the saveBtn in the FragmentExerciseList.kt
    -Do the logic for the other screens in this app.

--- app/src/main/java/adapter/ItemAdapter.kt
+++ app/src/main/java/adapter/ItemAdapter.kt
@@ -1,17 +1,31 @@
 package adapter
 
-/*In dieser Klasse habe ich meinen Beispieldatensetz in den Adapter implementiert.
+/*DE:
+In dieser Klasse habe ich meinen Beispieldatensetz in den Adapter implementiert.
 * Meiner Adaptaer initialisiert die einzelnen Daten die dann in die View Elemente
 * geladen werden. Zudem werden auch die Layouts der Listen Elemente erstellt und
-* gebunden.*/
-
-/*Nachtrag: Der Adapter wurde mit einem zweiten ViewHolder implementiert.
+* gebunden.
+Nachtrag: Der Adapter wurde mit einem zweiten ViewHolder implementiert.
 * Je nach dem, ob der anzuzeigende Content eine Kraftübung ist, wird ein
 * anders aussehendes UI Item im RecyclerView anzeigt.
 *
 * Nachtrag: Der Adapter wurde mit einem dritten ViewHolder implementiert.
 * Dadurch soll die Liste von Krafttrainingsübungen angezeigt werden. Ich komme
 * leider nicht zur Listenansicht, da ich die Navigation nicht programmieren kann.*/
+
+/* EN:
+ In this class I have implemented my example data set in the adapter.
+*My adapter initializes the individual data which is then loaded into the view elements.
+*The layouts of the list elements are also created and bound.
+*bound.
+*Addendum:
+*The adapter was implemented with a second ViewHolder.
+*Depending on whether the content to be displayed a force exercise, a
+*different looking UI item is displayed in the RecyclerView.
+*Addendum:
+*The adapter has been implemented with a third ViewHolder.
+*This should display the list of strength training exercises.
+*/
 
 import android.annotation.SuppressLint
 import android.util.Log
